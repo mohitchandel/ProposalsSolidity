d01065f2c38d42920cc47baffec740d60c95cd4d
Mohit Chandel <45650327+mohitchandel@users.noreply.github.com>
2021-07-29 22:01:50 +0530

    init

---
+++ src/contracts/GovtProposal.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract GovtProposal {
+
+    struct Proposal {
+        string name;
+        uint votes;
+    }
+    Proposal private proposal;
+
+    function makeProposal(string memory _name) private{
+        proposal = Proposal(_name, 0);
+    }
+
+    function voteProposal() private{
+
+    }
+}
